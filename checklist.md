89e7f2d19e42494590526952855edaf986cc2cdd
venturions <alex.eduardo10@hotmail.com>
2026-05-04 21:08:50 -0300

    feat: implementação do desafio

---
+++ checklist.md
@@ -0,0 +1,13 @@
+# Tarefas
+
+Use este checklist para ajudar a organizar a sua entrega.
+
+- [X] Criar um diretório para o projeto e, dentro dele, o arquivo `main.go`
+- [X] No `main.go`, declarar o `package main` e a função `func main()`
+- [X] Adicionar a estrutura de dados `map` ou importar arquivo JSON com as taxas de conversão
+- [X] Implementar a leitura dos argumentos passados pela linha de comando
+- [X] Converter o argumento de valor (string) para um número (float64)
+- [X] Realizar o cálculo de conversão multiplicando o valor pela taxa encontrada
+- [X] Imprimir o resultado final no console, formatado com duas casas decimais
+- [X] Compilar o código, gerando um executável chamado `convert`
+- [X] Testar a aplicação com exemplos válidos (ex: `./convert 10 USD`)
--- main.go
+++ main.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type Taxes struct {
+	Base  string             `json:"base"`
+	Date  string             `json:"date"`
+	Rates map[string]float64 `json:"rates"`
+}
 
 func main() {
-	fmt.Println("hello, world!")
+	data, err := os.ReadFile("rates.json")
+
+	if err != nil {
+		fmt.Println("Erro ao tentar carregar as taxas.")
+		return
+	}
+
+	var taxes Taxes
+
+	err = json.Unmarshal(data, &taxes)
+
+	if err != nil {
+		fmt.Println("Erro ao converter os dados das taxas.")
+		return
+	}
+
+	args := os.Args
+
+	if len(args) != 3 {
+		fmt.Println("Para converter uma moeda, utilize o padrão de conversão abaixo:\n./convert [valor_em_brl] [moeda_destino]")
+		return
+	}
+
+	value, err := strconv.ParseFloat(args[1], 64)
+
+	if err != nil {
+		fmt.Println("Digite um valor válido.")
+		return
+	}
+
+	rate, ok := taxes.Rates[args[2]]
+
+	if !ok {
+		fmt.Printf("A moeda %s não existe.", args[2])
+		return
+	}
+
+	convertedValue := value * rate
+
+	fmt.Printf("O resultado da conversão é %.2f\n", convertedValue)
+
 }
